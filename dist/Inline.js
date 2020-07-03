9e0b8eee1aa2e8b298b2b3915f3c935451d6f508
Robin de Mourat <robin.demourat@gmail.com>
2020-07-03 17:36:08 +0200

    display glossary conditionnally

--- dist/Inline.js
+++ dist/Inline.js
@@ -7,23 +7,38 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /* eslint react/no-danger : 0 */
 const Inline = ({
   /*
-   * resource,
    * contextualizer,
    */
+  resource,
   contextualization,
   renderingMode,
   children
-}, {}) => {
+}, {
+  useGlossary = false
+}) => {
+  if (renderingMode === 'paged') {
+    return useGlossary ? _react.default.createElement("span", {
+      id: contextualization.id,
+      className: `peritext-contextualization inline glossary rendering-mode-${renderingMode}`,
+      href: `#glossary-item-${resource.id}`
+    }, children) : children;
+  }
+
   return _react.default.createElement("span", {
     id: contextualization.id,
     className: `peritext-contextualization inline glossary rendering-mode-${renderingMode}`
   }, children);
 };
 
+Inline.contextTypes = {
+  useGlossary: _propTypes.default.boolean
+};
 var _default = Inline;
 exports.default = _default;
--- src/Inline.js
+++ src/Inline.js
@@ -1,18 +1,29 @@
 /* eslint react/no-danger : 0 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Inline = ( {
 
   /*
-   * resource,
    * contextualizer,
    */
+  resource,
   contextualization,
   renderingMode,
   children,
-}, {
-} ) => {
+}, {useGlossary = false} ) => {
+  if (renderingMode === 'paged') {
+    return useGlossary ? (
+      <span
+        id={ contextualization.id }
+        className={ `peritext-contextualization inline glossary rendering-mode-${renderingMode}` }
+        href={`#glossary-item-${resource.id}`}
+      >
+        {children}
+      </span>
+    ) : children;
+  }
   return (
     <span
       id={ contextualization.id }
@@ -23,5 +34,8 @@ const Inline = ( {
   );
 };
 
+Inline.contextTypes = {
+  useGlossary: PropTypes.boolean,
+}
 
 export default Inline;
